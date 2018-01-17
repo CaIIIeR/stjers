1c320bb3856931ec0e5f3986e34b92bcdabab41f
stjers <stjers@ttu.ee>
2018-01-17 18:52:39 +0200

    First commit

---
+++ VeebiProject155251IAPB/WatchlistWebApplication/build/generated/jax-wsCache/service/WatchlistWebService/ee/ttu/idu0075/_2017/ws/wlt/FilmType.java
@@ -0,0 +1,175 @@
+
+package ee.ttu.idu0075._2017.ws.wlt;
+
+import java.math.BigInteger;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for filmType complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="filmType"&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="id" type="{http://www.w3.org/2001/XMLSchema}integer"/&gt;
+ *         &lt;element name="name" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
+ *         &lt;element name="year" type="{http://www.w3.org/2001/XMLSchema}integer"/&gt;
+ *         &lt;element name="genre" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
+ *         &lt;element name="durationInMinutes" type="{http://www.w3.org/2001/XMLSchema}integer"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "filmType", propOrder = {
+    "id",
+    "name",
+    "year",
+    "genre",
+    "durationInMinutes"
+})
+public class FilmType {
+
+    @XmlElement(required = true)
+    protected BigInteger id;
+    @XmlElement(required = true)
+    protected String name;
+    @XmlElement(required = true)
+    protected BigInteger year;
+    @XmlElement(required = true)
+    protected String genre;
+    @XmlElement(required = true)
+    protected BigInteger durationInMinutes;
+
+    /**
+     * Gets the value of the id property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link BigInteger }
+     *     
+     */
+    public BigInteger getId() {
+        return id;
+    }
+
+    /**
+     * Sets the value of the id property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link BigInteger }
+     *     
+     */
+    public void setId(BigInteger value) {
+        this.id = value;
+    }
+
+    /**
+     * Gets the value of the name property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets the value of the name property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setName(String value) {
+        this.name = value;
+    }
+
+    /**
+     * Gets the value of the year property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link BigInteger }
+     *     
+     */
+    public BigInteger getYear() {
+        return year;
+    }
+
+    /**
+     * Sets the value of the year property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link BigInteger }
+     *     
+     */
+    public void setYear(BigInteger value) {
+        this.year = value;
+    }
+
+    /**
+     * Gets the value of the genre property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getGenre() {
+        return genre;
+    }
+
+    /**
+     * Sets the value of the genre property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setGenre(String value) {
+        this.genre = value;
+    }
+
+    /**
+     * Gets the value of the durationInMinutes property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link BigInteger }
+     *     
+     */
+    public BigInteger getDurationInMinutes() {
+        return durationInMinutes;
+    }
+
+    /**
+     * Sets the value of the durationInMinutes property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link BigInteger }
+     *     
+     */
+    public void setDurationInMinutes(BigInteger value) {
+        this.durationInMinutes = value;
+    }
+
+}
